aad7b2e14a372de33acb9a3f419e19c38cddbcc6
WoIfram <wolframep@yandex.ru>
2016-12-31 03:57:06 +0400

    Update resources.py

--- gentle/resources.py
+++ gentle/resources.py
@@ -1,11 +1,10 @@
-import logging
 import os
 
 from util.paths import get_resource, ENV_VAR
 from gentle import metasentence
 
-class Resources():
 
+class Resources:
     def __init__(self):
         self.proto_langdir = get_resource('PROTO_LANGDIR')
         self.nnet_gpu_path = get_resource('data/nnet_a_gpu_online')
@@ -14,7 +13,6 @@ class Resources():
         def require_dir(path):
             if not os.path.isdir(path):
                 raise RuntimeError("No resource directory %s.  Check %s environment variable?" % (path, ENV_VAR))
-
 
         require_dir(self.proto_langdir)
         require_dir(self.nnet_gpu_path)
